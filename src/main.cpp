8ba750523c2cd6f88491beda1c69cae55e063b7e
luiz-schmitz <schmitz.luizg@gmail.com>
2026-07-18 20:36:16 -0300

    feat: validate argument count before accessing argv

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
-int main() {
-    std::cout << "apexdyn: initial build OK" << std::endl;
+int main(int argc, char* argv[]){
+    if (argc < 2) {
+        std::cout << "usage: apexdyn <command> [args...]" << std::endl;
+        return 1;
+    }
+    std::cout << "argc: " << argc << ". argv[1]: " << argv[1] << std::endl;
     return 0;
 }
