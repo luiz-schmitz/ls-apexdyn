f5045f162049c92c2de6a63ca8ec7ec360cc5cf1
luiz-schmitz <schmitz.luizg@gmail.com>
2026-07-18 21:05:45 -0300

    feat: count .ini and .lut files in valid data directory

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,22 @@ int main(int argc, char* argv[]) {
     }else if (!std::filesystem::is_directory(dataDir)){
         std::cout << "error: path is not a directory: " << dataDir << std::endl;
         return 1;
-    }else {
-        std::cout << "path exists: " << dataDir << std::endl;
+    } else {
+        int iniCount = 0;
+        int lutCount = 0;
+
+        for (const auto& entry : std::filesystem::directory_iterator(dataDir)) {
+            if (entry.is_regular_file()) {
+                if (entry.path().extension() == ".ini") {
+                    iniCount++;
+                } else if (entry.path().extension() == ".lut") {
+                    lutCount++;
+                }
+            }
+        }
+        std::cout << "found " << iniCount << " .ini file(s) and "
+                  << lutCount << " .lut file(s) in " << dataDir << std::endl;
     }
+
     return 0;
 }
