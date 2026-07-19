dfa7e90c9a8bae9eb27ca0e851fda0618e6aad8e
luiz-schmitz <schmitz.luizg@gmail.com>
2026-07-18 21:00:40 -0300

    feat: validate that data directory argument exists and is a directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <filesystem>
 
-int main(int argc, char* argv[]){
-    if (argc < 2) {
-        std::cout << "usage: apexdyn <command> [args...]" << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        std::cout << "usage: apexdyn <command> <data-directory>" << std::endl;
         return 1;
     }
-    std::cout << "argc: " << argc << ". argv[1]: " << argv[1] << std::endl;
+    std::filesystem::path dataDir(argv[2]);
+    if (!std::filesystem::exists(dataDir)) {
+        std::cout << "error: path does not exist: " << dataDir << std::endl;
+        return 1;
+    }else if (!std::filesystem::is_directory(dataDir)){
+        std::cout << "error: path is not a directory: " << dataDir << std::endl;
+        return 1;
+    }else {
+        std::cout << "path exists: " << dataDir << std::endl;
+    }
     return 0;
 }
